eebb7646a25a59d5d7b23692eafc392d73d708c7
Richard Hartmann <cimatosa@gmx.de>
2016-10-04 17:15:49 +0200

    cleanup

--- stocproc/__init__.py
+++ stocproc/__init__.py
@@ -1,11 +1,5 @@
-# from . import stocproc_c as c
-# from .stocproc import *
-# 
-# from .class_stocproc_kle import *
-# from .class_stocproc import StocProc_FFT
-# from .class_stocproc import StocProc_KLE
-# 
-# import gquad
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from . import stocproc_c
 from . import stocproc
