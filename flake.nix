b7c6d760d34bde660402fe733d1617acf0692b74
Valentin Boettcher <hiro@protagon.space>
2021-12-09 14:47:19 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -5,11 +5,10 @@
   inputs = {
     utils.url = "github:vale981/hiro-flake-utils";
     nixpkgs.url = "nixpkgs/nixos-unstable";
-
   };
 
-  outputs = inputs@{ self, utils, nixpkgs, ... }:
-    (utils.lib.poetry2nixWrapper nixpkgs inputs {
+  outputs = { self, utils, nixpkgs, ... }:
+    (utils.lib.poetry2nixWrapper nixpkgs {
       name = "stocproc";
       poetryArgs = {
         projectDir = ./.;
