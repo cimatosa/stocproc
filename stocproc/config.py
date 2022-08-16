a8708b7018ffe477ccbaba91168bac1e6ff8c7d6
Valentin Boettcher <hiro@protagon.space>
2022-08-16 10:59:04 +0200

    do not break in absence of a `stocproc.ini`

--- stocproc/config.py
+++ stocproc/config.py
@@ -4,4 +4,4 @@ config = configparser.ConfigParser()
 config.read("stocproc.ini")
 
 
-USE_NORMALIZED_DIFF = config["FFT"].getboolean("use_normalized_diff", fallback=False)
+USE_NORMALIZED_DIFF = config.getboolean("FFT", "use_normalized_diff", fallback=False)
